879e26a767064605105822573cedae9074f332c5
medrimonia <medrimonia@gmail.com>
2016-11-02 23:11:19 +0100

    aadding extra functions to orthogonalSplit (splitSpace + splitSamples)

--- include/rosban_regression_forests/core/orthogonal_split.h
+++ include/rosban_regression_forests/core/orthogonal_split.h
@@ -12,9 +12,20 @@ public:
 
   OrthogonalSplit();
   OrthogonalSplit(int dim, double val);
+  OrthogonalSplit(const OrthogonalSplit & other);
 
   /* Return true if input belongs to the lower-space */
   bool isLower(const Eigen::VectorXd &input) const;
+
+  /// Separate all elements of input (one column is one input)
+  void splitEntries(const Eigen::MatrixXd & input,
+                    Eigen::MatrixXd * lower_output,
+                    Eigen::MatrixXd * upper_output);
+
+  /// Generate the lower and upper space
+  void splitSpace(const Eigen::MatrixXd & space,
+                  Eigen::MatrixXd * lower_space,
+                  Eigen::MatrixXd * upper_space);
 };
 }
 
